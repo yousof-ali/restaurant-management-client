ab15253370e8b57032554d23f2076b89f87b04c2
yousof-ali <yousofali293@gmail.com>
2025-02-12 20:30:21 +0600

    authentication page design

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,9 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Lottie from "lottie-react";
+import { FaRegEyeSlash } from "react-icons/fa6";
+import { FaRegEye } from "react-icons/fa6";
+import SignUp from "../../assets/signUp.json"
 
 const SignIn = () => {
+    const [err, setError] = useState("");
+    const [hide, setHide] = useState(true);
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        setError('')
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password)
+
+
+        // createUser(email, password)
+        //     .then(result => {
+        //         navigate(from);
+        //     })
+        //     .catch((err) => {
+        //         console.log(err.message);
+        //         setError(err.message);
+        //     });
+    };
+
+    const handlePasswordHide = () => {
+        setHide(!hide)
+    }
+
     return (
-        <div>
-            signin
+        <div className="min-h-[80vh] bg-white  flex items-center justify-center py-10 px-4">
+            <div className="flex flex-col md:flex-row  items-center max-w-[1400px] w-full rounded-xl overflow-hidden ">
+
+
+                <div className="w-full md:w-1/2 p-6 md:p-10">
+                    <h1 className="text-4xl font-bold text-center mb-2">Sign In</h1>
+                    <p className='text-center text-gray-500 lg:mb-12 mb-6'>Log In your Delish Account</p>
+
+                    <div className='flex  gap-4 justify-between'>
+                        <div className='w-1/2 mb-2'>
+                            <button className='border duration-500 hover:bg-gray-200 border-gray-300 rounded-md flex gap-2 py-3 items-center justify-center cursor-pointer bg-gray-100 text-xl w-full '><img className='w-8' src="./google.png" alt="" />Google</button>
+                        </div>
+                        <div className='w-1/2'>
+                            <button className='border duration-500 hover:bg-gray-200 border-gray-300 rounded-md flex gap-2 py-3 items-center justify-center cursor-pointer bg-gray-100 text-xl w-full '><img className='w-8' src="./facebook.png" alt="" />Facebook</button>
+                        </div>
+                    </div>
+
+                    <div className='divider divider-accent'> Or </div>
+
+                    <form onSubmit={handleLogin} className='space-y-4'>
+
+                        <div className='form-control '>
+                            <label className="ml-2 mb-1 text-sm font-medium">Email</label>
+                            <input
+                                type="email"
+                                name='email'
+                                className="block w-full px-4 py-3 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500"
+                                placeholder="Enter your Email"
+                                required
+                            />
+                        </div>
+                        <div className='form-control relative  mt-4 lg:mt-0'>
+                            <label className="ml-2 mb-1 text-sm font-medium">Password</label>
+                            <input
+                                type={hide ? 'password' : 'text'}
+                                name='password'
+                                className="block w-full px-4 py-3 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500"
+                                placeholder="Password"
+                                required
+                            />
+                            <div className='text-2xl top-9 right-6 absolute' onClick={handlePasswordHide}>
+                                {
+                                    hide ? <FaRegEye />
+
+                                        : <FaRegEyeSlash />
+
+                                }
+                            </div>
+
+                            <div className='flex justify-end'>
+                            <button className='btn  btn-link  text-gray-500'>Forget Password ?</button>
+                            </div>
+
+
+                        </div>
+                        
+                        {err && <p className='text-sm text-red-500 '>{err}</p>}
+
+                        <button className="w-full  bg-green-500 text-white py-3 rounded-full hover:bg-green-600 transition duration-300">Sign In</button>
+                    </form>
+                    <p className='mt-2 ml-2 text-gray-500'>You Have No Account? <Link to={'/sign-up'} className='btn btn-link text-red-500'>Sign Up</Link> </p>
+                </div>
+                <div className="hidden md:block md:w-1/2">
+                    <Lottie animationData={SignUp} className="w-full h-full" />
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,9 +1,122 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Lottie from "lottie-react";
+import signUp from '../../assets/signUp.json';
+import CommonButton from '../../Components/CommonButton'
+import { Link } from 'react-router-dom';
+import { FaRegEyeSlash } from "react-icons/fa6";
+import { FaRegEye } from "react-icons/fa6";
+
+
+
 
 const SignUp = () => {
+    const [err, setError] = useState("");
+    const [hide, setHide] = useState(true);
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        setError('');
+        const name = e.target.name.value;
+        const photo = e.target.photo.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name, photo, email, password)
+
+        if (password.length < 6) {
+            setError('Password must be at least 6 characters long!');
+            return;
+        } else if (!/^(?=.*[A-Z])/.test(password)) {
+            setError('Password must include at least one uppercase letter!');
+            return;
+        }
+
+        // createUser(email, password)
+        //     .then(result => {
+        //         navigate(from);
+        //     })
+        //     .catch((err) => {
+        //         console.log(err.message);
+        //         setError(err.message);
+        //     });
+    };
+
+    const handlePasswordHide = () => {
+        setHide(!hide)
+    }
+
     return (
-        <div>
-            SignUp
+        <div className="min-h-[80vh] bg-white  flex items-center justify-center py-10 px-4">
+            <div className="flex flex-col md:flex-row  items-center max-w-[1400px] w-full rounded-xl overflow-hidden ">
+
+
+                <div className="w-full md:w-1/2 p-6 md:p-10">
+                    <h1 className="text-4xl font-bold text-center mb-2">Sign Up</h1>
+                    <p className='text-center text-gray-500 lg:mb-12 mb-6'>Create your Delish Account</p>
+
+                    <form onSubmit={handleRegister} className='space-y-4'>
+                        <div className='lg:flex gap-4 '>
+                            <div className='form-control lg:w-1/2'>
+                                <label className="ml-2 mb-1 text-sm font-medium">Name</label>
+                                <input
+                                    type="text"
+                                    name='name'
+                                    className="block w-full px-4 py-3 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500"
+                                    placeholder="Your Name"
+                                    required
+                                />
+                            </div>
+                            <div className='form-control mt-4 lg:mt-0 lg:w-1/2'>
+                                <label className="ml-2 mb-1 text-sm font-medium">Photo</label>
+                                <input
+                                    type="url"
+                                    name='photo'
+                                    className="block w-full px-4 py-3 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500"
+                                    placeholder="Photo URL"
+                                />
+                            </div>
+                        </div>
+
+                        <div className='lg:flex gap-4 '>
+                            <div className='form-control lg:w-1/2'>
+                                <label className="ml-2 mb-1 text-sm font-medium">Email</label>
+                                <input
+                                    type="email"
+                                    name='email'
+                                    className="block w-full px-4 py-3 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500"
+                                    placeholder="Enter your Email"
+                                    required
+                                />
+                            </div>
+                            <div className='form-control relative  mt-4 lg:mt-0 lg:w-1/2'>
+                                <label className="ml-2 mb-1 text-sm font-medium">Password</label>
+                                <input
+                                    type={hide?'password':'text'}
+                                    name='password'
+                                    className="block w-full px-4 py-3 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-500"
+                                    placeholder="Password"
+                                    required
+                                />
+                                <div className='text-2xl top-9 right-6 absolute' onClick={handlePasswordHide}>
+                                    {
+                                        hide ? <FaRegEye /> 
+
+                                            : <FaRegEyeSlash />
+
+                                    }
+                                </div>
+                            </div>
+                        </div>
+
+                        {err && <p className='text-sm text-red-500 '>{err}</p>}
+
+                        <button className="w-full  bg-green-500 text-white py-3 rounded-full hover:bg-green-600 transition duration-300">Register</button>
+                    </form>
+                    <p className='mt-2 ml-2 text-gray-500'>Already Have Account? <Link to={'/sign-in'} className='btn btn-link text-red-500'>Sign In</Link> </p>
+                </div>
+                <div className="hidden md:block md:w-1/2">
+                    <Lottie animationData={signUp} className="w-full h-full" />
+                </div>
+            </div>
         </div>
     );
 };
